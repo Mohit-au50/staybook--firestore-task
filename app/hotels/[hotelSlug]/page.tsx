bc0be4abf5f15c86e2e34b98865fc6023af4953e
AbhayThakurr <95166645+AbhayThakurr@users.noreply.github.com>
2024-05-31 14:32:09 +0530

    Changes in update hotel

--- app/hotels/[hotelSlug]/page.tsx
+++ app/hotels/[hotelSlug]/page.tsx
@@ -1,28 +1,34 @@
 
-"use client"
+
+"use client";
 
 import React, { useState, useEffect } from 'react';
 import HotelForm from '../../components/HotelForm';
 import { useRouter } from 'next/navigation';
 import { db } from '../../../lib/firebase/firebaseConfig';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 import { HotelDetails } from '../../../lib/classes/hotelDetails';
 import handleUpdate from '@/lib/firebase/update/handleUpdate';
 
+// Define the props type
+interface UpdateHotelProps {
+  params: {
+    hotelSlug: string;
+  };
+}
 
-const UpdateHotel: React.FC = ({params}) => {
-  const router = useRouter()
+const UpdateHotel: React.FC<UpdateHotelProps> = ({ params }) => {
+  const router = useRouter();
   const [initialData, setInitialData] = useState<HotelDetails | null>(null);
 
   // get id for dynamic routing
   const id = params.hotelSlug;
 
-
-// fetch the data
+  // fetch the data
   useEffect(() => {
     const fetchHotel = async () => {
       if (id) {
-        const hotelDoc = await getDoc(doc(db, 'hotels', id as string));
+        const hotelDoc = await getDoc(doc(db, 'hotels', id));
         if (hotelDoc.exists()) {
           setInitialData(hotelDoc.data() as HotelDetails);
         } else {
@@ -34,14 +40,14 @@ const UpdateHotel: React.FC = ({params}) => {
     fetchHotel();
   }, [id]);
 
-//updateing the data
+  // updating the data
   const handleSubmit = async (updatedData: HotelDetails) => {
     try {
       if (id) {
         const success = await handleUpdate(id, updatedData);
         if (success) {
           alert('Hotel updated successfully');
-          router.push("/hotels")
+          router.push("/hotels");
         } else {
           alert('Failed to update hotel');
         }
@@ -51,15 +57,13 @@ const UpdateHotel: React.FC = ({params}) => {
     }
   };
 
-  
-  
   if (!initialData) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h1 className=' text-center'>Update Hotel</h1>
+      <h1 className='text-center'>Update Hotel</h1>
       <HotelForm onSubmit={handleSubmit} initialData={initialData} />
     </div>
   );
